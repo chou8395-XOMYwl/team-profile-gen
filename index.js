7ab584bec715f6e693c577c909ce91ad5bff1ee2
chou8395-XOMYwl <chou8395@gmail.com>
2022-10-13 11:29:55 -0400

    changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
-const generateHTML = require('./src/generateHTML')
-
+const inquirer = require("inquirer");
+const generateHTML = require('./src/genHTML')
 
 const Intern = require('./lib/Intern'); 
 const Manager = require('./lib/Manager');
@@ -97,6 +96,78 @@ const addEmployee = () => {
     })
 
 };
+
+const engineerInput = () => {
+    
+    return inquirer.prompt ([
+         {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?", 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the employee's name.");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's ID.",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ("Please enter the employee's ID.")
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter email.')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the employee's GitHub username.",
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Do you want to add more team members?',
+            default: false
+        }
+    ])
+    .then(engineerInput => {
+        console.log(engineerInput)
+        const  {name, id, email, github, confirmAddEmployee} = engineerInput; 
+        const engineer = new Engineer (name, id, email, github);
+
+        pageArray.push(engineer); 
+        if(confirmAddEmployee){
+            addEmployee();
+        } else {
+            console.log(pageArray)
+            const html = generateHTML(pageArray) 
+            writeFile(html)
+        }
+        
+    })
+}
 
 const internInput = () => {
 
